c2dcba3c27074f4464782877ca8010967beab3cf
Maksym Nakonechnyi <maksimnakonechij59@gmail.com>
2025-10-26 16:16:46 +0100

    Pretty gross commit, initial state of GUI project, already contains some XAML layout

---
+++ src/PlaceholderService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Task_Manager_GUI
+{
+    public static class PlaceholderService
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.RegisterAttached("PlaceholderText", typeof(string), typeof(PlaceholderService), new PropertyMetadata(String.Empty));
+        
+        public static void SetPlaceholderText(UIElement element, string value) =>
+            element.SetValue(PlaceholderTextProperty, value);
+
+        public static string GetPlaceholderText(UIElement element) =>
+            (string)element.GetValue(PlaceholderTextProperty);
+
+        public static readonly DependencyProperty PlaceholderFontSizeProperty =
+            DependencyProperty.RegisterAttached("PlaceholderFontSize", typeof(double), typeof(PlaceholderService), new PropertyMetadata(14.0));
+
+        public static void SetPlaceholderFontSize(UIElement element, double value) =>
+            element.SetValue(PlaceholderFontSizeProperty, value);
+
+        public static double GetPlaceholderFontSize(UIElement element) =>
+            (double)element.GetValue(PlaceholderFontSizeProperty);
+        
+    }
+}
---
+++ src/Windows/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_Manager_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddTaskWindow addTaskWindow = new AddTaskWindow();
+            addTaskWindow.Owner = this;
+            addTaskWindow.ShowDialog();
+        }
+
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            //SettingsWindow settingsWindow = new SettingsWindow();
+            //settingsWindow.Owner = this;
+            //settingsWindow.ShowDialog();
+
+            //var dialog = new OpenFileDialog
+            //{
+            //    Title = "Select a file",
+            //    Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+            //};
+
+            //bool? result = dialog.ShowDialog();
+
+            //if (result == true)
+            //{
+            //    string filepath = dialog.FileName;
+
+            //    using (FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            //    {
+            //        fs.Write(Encoding.UTF8.GetBytes("Hello, World!"), 0, "Hello, World!".Length);
+            //    }
+
+            //}
+        }
+
+    }
+}
